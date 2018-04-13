caf61246414b3dbc65f0eaa403ab53507ca97559
Carter Rhea <carterrhea93@gmail.com>
2018-04-13 12:23:21 -0400

    Added some documentation

--- NHLDriveTimes/Main.py
+++ NHLDriveTimes/Main.py
@@ -2,19 +2,20 @@ from Map import map
 from CreateDistanceMatrix import distance_matrix_make
 from CalculateShortestRoute import shortest_route_calculation
 from Time import calculate_time
-create_dist_mat = False  #only if not already made
-calc_shortest = False
-map_it = False
-calc_time = True
+#--------------------INPUT PARAMETERS-------------------#
+create_dist_mat = False  # Create Distance Matrix
+calc_shortest = False # Calculate Distances
+map_it = False # Pretty Map
+calc_time = True # Total Drive time for shortest route
 file_ext = 'nhl'
-
-
+#-------------------------------------------------------#
+#--------------List of Locations------------------------#
 Atlantic_div = ['Boston, MA','Buffalo, NY','Detroit, MI','Miami, FL','Montreal, QC, Canada','Ottawa, ON, Canada','Tampa, FL','Toronto, ON, Canada']
 Metro_div = ['Raleigh, NC','Columbus, OH','East Rutherford, NJ','New York, NY','Philadelphia, PA','Pittsburgh, PA','Washington D.C.']
 Central_div = ['Chicago, IL','Denver, CO','Dallas, TX','Minneapolis, MN','Nashville, TN','St. Louis, MO','Winnipeg, MB, Canada']
 Pacific_div = ['Anaheim, CA','Glendale, AZ','Calgary, AB, Canada','Edmonton, AB','Los Angeles, CA','San Jose, CA','Vancouver, BC, Canada','Paradise, NV']
 NHLTeams = Atlantic_div+Metro_div+Central_div+Pacific_div
-
+#-------------------------------------------------------#
 
 
 def main():
--- NHLDriveTimes/ReadME.md
+++ NHLDriveTimes/ReadME.md
@@ -1,5 +1,7 @@
 The goal of this project is to calculate the shortest possible distance one would need to travel in order to visit all NHL teams.
 I use google map API to calculate the distances and from there I simply scan for the shortest route.
 
+Simply set parameters detailed in Main.py and hit go!
+
 Things to add:
   Change alphanumeric markers to alpha-color markers
